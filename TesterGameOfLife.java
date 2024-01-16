74c62af9ff5e952fc314914c822926c5a123a6fc
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2024-01-16 21:27:32 +0200

    Little Fixes

--- TesterGameOfLife.java
+++ TesterGameOfLife.java
@@ -1,13 +1,13 @@
 public class TesterGameOfLife {
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java TesterGameOfLife <functionName> <filename>");
-            return;
-        }
+        // if (args.length < 2) {
+        //     System.out.println("Usage: java TesterGameOfLife <functionName> <filename>");
+        //     return;
+        // }
 
-        String functionName = args[0];
-        String fileName = args[1];
+        String functionName = "print";
+        String fileName = "pulsar.dat";
 
         switch (functionName) {
             case "print":
